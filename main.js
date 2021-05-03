d63fa5a568ab6b99573b1fd18762349a952ed01b
codemantra10 <73803525+codemantra10@users.noreply.github.com>
2021-05-02 20:49:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+object="";
 Webcam.set({
 height:490,
 width:490,
@@ -12,7 +12,26 @@ document.getElementById("result").innerHTML="<img src='"+data_uri+"'id='ifs'>";
 })
 }
 console.log("ml5version",ml5.version)
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/DPWtOmq5L/model.json",modelloaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/6OzYz_jAU/model.json",modelloaded);
 function modelloaded(){
-console.log("My model will crush you and make history!I will be famous in class!")
+console.log("My model will crush you and make history!I will be famous in");
 }
+function analyze(){
+img=document.getElementById("ifs");
+classifier.classify(img,gotresult);
+}
+function gotresult(error,result){
+if (error) {
+console.error(error);
+}
+else{
+console.log(result)
+confidence=result[0].confidence;
+secondstage=confidence.toFixed(3);
+finalresult=secondstage*100;
+document.getElementById("accuracy_value").innerHTML=finalresult+"%";
+object=result[0].label;
+document.getElementById("object_name").innerHTML=object+" is being shown in the image"
+}
+}
+
